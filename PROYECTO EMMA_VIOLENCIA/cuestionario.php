b777b188a946c2de0438763133c463e2c667e6c6
GimenaAngel <119822583+GimenaAngel@users.noreply.github.com>
2022-12-04 18:28:08 -0600

    REVISTA DIGITAL

---
+++ PROYECTO EMMA_VIOLENCIA/cuestionario.php
@@ -0,0 +1,197 @@
+﻿<html>
+<head>
+<title>Violencia-Cuestionario</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<Style>
+body{
+ background-image:url("descarga.jpg");
+}
+</Style>
+<meta name="Classification" content="art, business">
+<meta name="Classification" content="art, business">
+<meta name="KeyWords" content="corporations, corporaciones, activism, activismo, mejor vida, social, campañas, campains, cards, id, student, estudiantes, credenciales, streets, calles, mexican, world wide, free, gratis, souvenirs, homeless, statistics, estadísticas, violencia, violence, global, barcode, codigo de barras, tear gas, lacrimógeno, semillas, boletos, radio, public radio, df, gobierno, ong, ngo, corrupción, corruption, lotto, lotería, donaciones, donations, conceptual, art, arte, subway, metro, mexico city, ciudad de mexico,  streets, calles, interventions, intervenciones, productos, products, services, servicios, latinamerican tower, torre latinoamericana, minerva cuevas">
+
+<body bgcolor="#FFFFFF" text="#000000" link="#000000" vlink="#000000" alink="#000000">
+<center>
+  <tr bgcolor="#000000"> 
+    <td> 
+      <p><b><font color="#999999"><font size="+3" face="Arial, Helvetica, sans-serif" color="#FFFFFF">E</font><font size="+2" face="Arial, Helvetica, sans-serif" color="#FFFFFF">NCUESTA 
+        </font><font size="+3" face="Arial, Helvetica, sans-serif" color="#FFFFFF">S</font><font size="+2" face="Arial, Helvetica, sans-serif" color="#FFFFFF">OBRE 
+        </font><font size="+3" face="Arial, Helvetica, sans-serif" color="#FFFFFF">V</font><font size="+2" face="Arial, Helvetica, sans-serif" color="#FFFFFF">IOLENCIA</font></font></b> 
+      </p>
+      <p><font face="Arial, Helvetica, sans-serif" color="#FFFFFF" size="2">Nuestro 
+        representante aplica este cuestionario en lugares p&uacute;blicos, se 
+        le pide al entrevistado una lista de nombres de personas violentas que 
+        conozcan incluyendo datos de contacto. El representante aplicar&aacute; 
+        este mismo cuestionario a las personas dadas como referencia. Aqu&iacute; 
+        usted puede participar de esta encuesta.</font> 
+    </center></td>
+  </tr>
+</table>
+<form method="post" action="mailto:mvc@irational.org" name="violencia">
+  <p><font color="#000000"><font size="4" face="Arial, Helvetica"><b><center>CUESTIONARIO</b></font></font></p>
+  <p><font color="#000000"><b><font size="2" face="Arial, Helvetica">1) Cree que 
+    la violencia es:</font></b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="1" value="radiobutton1" checked>
+    no existe 
+    <input type="radio" name="1" value="radiobutton2">
+    necesaria 
+    <input type="radio" name="1" value="radiobutton3">
+    ocasional 
+    <input type="radio" name="1" value="radiobutton4">
+    provocada 
+    <input type="radio" name="1" value="radiobutton5">
+    un castigo 
+    <input type="radio" name="1" value="radiobutton6">
+    cotidiana <br>
+    <input type="radio" name="1" value="radiobutton7">
+    otro 
+    <input type="text" name="otro" maxlength="20">
+    </font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>2) ¿Alguna vez 
+    ha sido violento con alguna persona cercana a usted?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="2" value="radiobutton8" checked>
+    familiar 
+    <input type="radio" name="2" value="radiobutton9">
+    amigo 
+    <input type="radio" name="2" value="radiobutton10">
+    no</font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>3) ¿Alguna persona 
+    cercana ha sido violenta con usted?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="3" value="radiobutton11" checked>
+    familiar 
+    <input type="radio" name="3" value="radiobutton12">
+    amigo 
+    <input type="radio" name="3" value="radiobutton13">
+    no</font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>4) ¿Alguna vez 
+    ha sido violento con alguna persona lejana a usted?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="4" value="radiobutton14" checked>
+    conocido 
+    <input type="radio" name="4" value="radiobutton15">
+    desconocido 
+    <input type="radio" name="4" value="radiobutton16">
+    no</font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>5) ¿Alguna persona 
+    lejana a ha sido violenta con usted?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="5" value="radiobutton17" checked>
+    conocido 
+    <input type="radio" name="5" value="radiobutton18">
+    desconocido 
+    <input type="radio" name="5" value="radiobutton19">
+    no</font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>6) ¿Fue golpeado 
+    usted de pequeño?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="6" value="radiobutton20" checked>
+    demasiado 
+    <input type="radio" name="6" value="radiobutton21">
+    mucho 
+    <input type="radio" name="6" value="radiobutton22">
+    poco 
+    <input type="radio" name="6" value="radiobutton23">
+    nada </font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>7) ¿Cree que las 
+    personas fuertes son las que generalmente cometen alguna acción violenta contra 
+    los débiles?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="7" value="radiobutton24" checked>
+    si 
+    <input type="radio" name="7" value="radiobutton25">
+    no </font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>8) Sería usted 
+    violento con alguien si es que lo provocan?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="8" value="radiobutton26" checked>
+    si aunque no exista provocación 
+    <input type="radio" name="8" value="radiobutton27">
+    si 
+    <input type="radio" name="8" value="radiobutton28">
+    no </font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>9) ¿Ha provocado 
+    alguna vez a alguien y ha recibido una respuesta violenta?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="9" value="radiobutton29" checked>
+    si 
+    <input type="radio" name="9" value="radiobutton30">
+    no </font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>10)¿Cree que la 
+    violencia siempre tiene un motivo?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="10" value="radiobutton31" checked>
+    si 
+    <input type="radio" name="10" value="radiobutton32">
+    no </font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>11) Cree que la 
+    violencia es más frecuente en...</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="11" value="radiobutton33" checked>
+    hogar 
+    <input type="radio" name="11" value="radiobutton34">
+    trabajo 
+    <input type="radio" name="11" value="radiobutton35">
+    escuela 
+    <input type="radio" name="11" value="radiobutton36">
+    calle/transporte público 
+    <input type="radio" name="11" value="radiobutton37">
+    centros de entretenimiento</font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>12) ¿Conoce personalmente 
+    a personas violentas?</b></font></p>
+  <p> <font size="1" face="Arial, Helvetica" color="#000000"> 
+    <input type="radio" name="12" value="radiobutton38" checked>
+    si 
+    <input type="radio" name="12" value="radiobutton39">
+    no </font></p>
+  <p><font size="2" face="Arial, Helvetica" color="#000000"><b>Si su respuesta 
+    a la &uacute;ltima pregunta es afirmativa, ay&uacute;denos a enviar este mismo 
+    cuestionario a la persona(s) violenta(s) que usted conoce.</b></font></p>
+  <p><font size="1" face="Arial, Helvetica" color="#000000">Nombre de la persona 
+    violenta: 
+    <input type="text" name="nombre">
+    </font></p>
+  <p><font size="1" face="Arial, Helvetica" color="#000000"> Relaci&oacute;n: 
+    <select name="relacion">
+      <option>Padre</option>
+      <option>Madre</option>
+      <option>Hermano (a)</option>
+      <option>Otro Familiar</option>
+      <option>Novio (a)</option>
+      <option>Esposo</option>
+      <option>Amigo</option>
+      <option>Vecino</option>
+      <option selected>Otro</option>
+    </select>
+    </font></p>
+  <p><font size="4" color="#000000" face="Arial, Helvetica"><b>*</b></font><font size="1" face="Arial, Helvetica" color="#000000">Correo 
+    Electr&oacute;nico: 
+    <input type="text" name="correo">
+    </font></p>
+  <p><font size="4" color="#000000" face="Arial, Helvetica"><b>*</b></font><font size="1" face="Arial, Helvetica" color="#000000">Direcci&oacute;n: 
+    <textarea name="dir" cols="25" rows="5"></textarea>
+    </font></p>
+  <p><font size="4" color="#000000" face="Arial, Helvetica"><b>*</b></font><font size="1" face="Arial, Helvetica" color="#000000">Tel&eacute;fono: 
+    <input type="text" name="tel" maxlength="20">
+    </font></p>
+  <p><font size="4" color="#000000" face="Arial, Helvetica"><b>*</b></font><font size="1" color="#000000" face="Arial, Helvetica">Debe 
+    proporcionarnos al menos una opci&oacute;n de contacto.</font></p>
+  <p> <font size="3" color="#000000"> 
+    <input type="submit" name="enviar" value="Enviar">
+    <input type="reset" name="borrar" value="Borrar/Nuevo">
+    </font></p>
+</form>
+<p><font face="Arial, Helvetica" size="1">comentarios: <a href="mailto:mvc@irational.org">gime8765dal@gmail.com</a></font></p></center>
+
+<br>
+<br>
+
+<a href="REVISTADIGITAL.HTML"><center>
+    <button>REGRESAR AL INICIO</button></center>
+  </a> 
+</body>
+</html>
